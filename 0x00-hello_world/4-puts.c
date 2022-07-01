fbadb32b1553c4c3320d432c223d5de86e7c041c
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-06-30 20:01:19 -0700

    C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * print word to output
+ * Return always 0(success)
+ */
+
+int main(void) {
+	puts("\"Pogramming is like buildin a multilingual puzzle\n");
+	return (0);
+}
+		
