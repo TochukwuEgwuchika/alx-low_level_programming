ac5717030d06ebd347e8885b0c27b8e15c9391d3
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-04 14:05:15 +0100

    print size in bytes of int, float, etc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- main - Entry point
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
-Return: Always 0 (Success)
-/
 int main(void)
 {
 int a;
@@ -14,8 +14,9 @@ char d;
 float f;
 
 printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(d));
-printf("Size of an int: %lu bytes(s)\n",(unsigned long)sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n, (unsigned long)sizeof(c));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s) \n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
