6a84849ea611b0a8284c1559ef1cd0122148daf2
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-05 22:40:29 +0100

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main - prints _putchar
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char *c = "_putchar";
+
+	_putchar(c);
+	_putchar('\n');
+	return (0);
+}
