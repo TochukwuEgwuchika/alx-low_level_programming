ae0c1c794ee65038004d2b1b951c296be1ae997d
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-06 00:09:27 +0100

    3-is_lowe.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for a lowercase character
+ * Return: 1 if c is lowercase
+ * Otherwise: 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
+	
