4db4fd83cef4b5bae3a46aa1f09c6d7648415597
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-08 01:40:11 +0100

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 to 9)
+ * @c : variable to be checked
+ * Return: 1 if digit
+ * 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int retval;
+
+	if (c >= 0 && c <= 9)
+	{
+		retval = 1;
+	}
+	else
+	{
+		retval = 0;
+	}
+	return (retval);
+}
