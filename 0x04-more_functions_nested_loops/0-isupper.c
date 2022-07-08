03f3900720e32fea8fe93c9bf49963b971958159
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-08 01:18:33 +0100

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper: checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int retval;
+
+	if (_isupper(c))
+	{
+		retval = 1;
+	}
+	else
+	{
+		retval = 0;
+	}
+	return (retval);
+}
+
