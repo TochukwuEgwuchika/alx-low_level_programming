611cc518d2900ca2a472a26ace9add48907c3219
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-15 04:30:40 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: parameter
+ * @src: parameter
+ * Return: dest
+ */
+
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+
+	dest[i + j] = '\0';
+	return dest;
+}
