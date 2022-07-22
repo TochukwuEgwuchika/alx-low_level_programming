cb19b0f05170b3394eb2b1d588b3216cefec710b
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-22 22:24:09 +0100

    Program to print number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it
+ * @argc - parameter
+ * @argv - argument vector
+ */
+
+int main(int argc, char *argv[])
+{
+    printf("%d", argc);
+}
