b03ff75b423056efb39242b393cedd0803e2d047
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-22 22:33:25 +0100

    betty fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - prints the number of arguments passed to it
- * @argc: parameter
+ * main - prints out all of the arguments passed to it.
+ * @argc: argument count
  * @argv: argument vector
- * 
- * Return: 0
+ * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
-    printf("%d\n", argc - 1);
-    return (0);
+	printf("%d\n", argc - 1);
+	argv[0] = 0;
+	return (0);
 }
