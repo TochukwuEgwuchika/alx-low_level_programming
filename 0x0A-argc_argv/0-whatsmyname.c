005c4f47546322db40ebf0459fbda48d1f0e2da0
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-22 21:25:39 +0100

    A program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints program name
+ * argc: argument count
+ * argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s", argv[0]);
+}
