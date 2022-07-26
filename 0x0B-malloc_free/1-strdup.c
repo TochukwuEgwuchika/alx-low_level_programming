79c6cfa5463cf4789b104b2880fbf666bf855af9
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-26 22:15:50 +0100

    Malloc task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to be duplicated
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	char *new;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+
+	new = malloc(i * sizeof(char));
+
+	if (new  == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new = str;
+		return new;
+	}
+}
