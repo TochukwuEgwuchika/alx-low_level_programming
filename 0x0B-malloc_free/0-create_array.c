f957ae87fdf78890ed4a247b95de79081b025fa8
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-26 21:45:21 +0100

    Malloc task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,23 +16,23 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		{
-			return (NULL);
-		}
+	{
+		return (NULL);
+	}
 
-		s = malloc((size) * sizeof(char));
-				
-		if (s == NULL)
-		{
-			return (NULL);
-		}
-		i = 0;
+	s = malloc((size) * sizeof(char));
 
-		while (i < size)
-		{
-			s[i] = c;
-			i++;
-		}
-		s[i] = '\0';
-		return (s);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
 }
