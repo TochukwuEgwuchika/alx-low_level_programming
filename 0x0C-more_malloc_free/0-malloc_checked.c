a47e01c5c579b23c812c579bbf49b96b95a34a89
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-28 22:08:53 +0100

    Malloc task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+include "main.h'
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: space to be allocated
+ *
+ * Return: pointer is sucessful
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+	
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		return (98);
+	}
+	else
+	{
+		return p;
+	}
+}
