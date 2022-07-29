8b6b05cc32ba9eb431e6d9f58d39f6a2d205805b
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-07-29 04:37:45 +0100

    Malloc task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 /**
- * *malloc_checked - does what malloc does and checks if null
+ * malloc_checked - does what malloc does and checks if null
  * @b: size to alloc for malloc
  *
  * Return: void pointer to allocated mem
  */
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	void *a;
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	a = malloc(b);
+	if (a == NULL)
 		exit(98);
-	return (ptr);
+	return (a);
 }
