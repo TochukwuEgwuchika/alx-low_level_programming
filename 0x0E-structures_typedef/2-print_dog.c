7f0201281d708105c57ee372a5419f1ee4eda3d7
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-08-01 22:14:23 +0100

    Structures and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,5 +10,5 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	printf("Name: %s\nAge: %f\nOwner: %s", d->name == NULL? "(nil)" : d->name, d->age == NULL ? "(nil)" : d->age, d->owner == NULL ? "(nil)" : d->owner;
+	printf("Name: %s\nAge: %f\nOwner: %s", d->name == NULL ? "(nil)" : d->name, d->age == NULL ? "(nil)" : d->age, d->owner == NULL ? "(nil)" : d->owner;
 }
