4a140761515e1e5bc39da877fb0e804058f73c67
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-08-01 21:28:58 +0100

    Structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - a structure name dog
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ */
+
+struct dog 
+{
+	char* name;
+	float age;
+	char* owner;
+};
