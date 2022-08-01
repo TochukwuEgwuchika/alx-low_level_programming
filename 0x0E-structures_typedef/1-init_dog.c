91987aafec8fbaf7588a6c7322cc5eea20556848
Egwuchika Tochukwu <tobezonyeze34@gmail.com>
2022-08-01 21:51:27 +0100

    Structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog,h"
+/**
+ * init_dog - create dog
+ * @d: dog
+ * @name: dog name
+ * age: age
+ * owner: owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (void);
+}
